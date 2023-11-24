880f49ee0b448866a3bbbd3787d99b2245c9058f
Çağrı Sarıyıldız <58122280+csariyildiz@users.noreply.github.com>
2023-11-24 21:40:20 +0300

    Update and rename list-installed-programs.md to list-installed-programs.ps1

--- powershell/list-installed-programs.ps1
+++ powershell/list-installed-programs.ps1
@@ -1,11 +1,3 @@
-# Get Installed Programs With PowerShell
-
-While updating a server with an old operating system ( Windows Server 2008 R2 Standard ), we decided to replace it with another one with Windows 2019 Standart Edition.
-Naturally, all the programs on the old computer had to be installed on the new one. Meanwhile, I needed a list of the programs on the server.
-
-The following script lists the installed programs on the computer and saves it to a `CSV` file. The file named with computer name will be created with in the folder where you run the script. You can save it as a `.ps1` file and run.
-
-```
 Clear-Host;
 $computerName = Get-WMIObject Win32_ComputerSystem | Select-Object -ExpandProperty name;
 $outputFile = "$computerName.csv";
@@ -23,4 +15,3 @@ foreach ($application in $applications)
         Add-Content $outputFile "$displayName;$displayVersion;$publisher;$installDate";
     }
 }
-```
